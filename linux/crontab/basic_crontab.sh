dd7b413eee2a826642cfd9707371d25e757b7e22
ramesh1918s <shivaram915454@gmail.com>
2025-03-09 11:45:50 +0530

    crontab

---
+++ linux/crontab/basic_crontab.sh
@@ -0,0 +1,4 @@
+What is Crontab?
+Crontab (cron table) is a configuration file that specifies scheduled tasks (cron jobs)
+to run automatically at specified times or intervals on Unix-based systems.
+
